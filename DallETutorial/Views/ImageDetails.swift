04a3b4e99302e2a37b516e67661772085147d6f6
Oleg <granchenkooleg@gmail.com>
2023-07-07 16:24:38 +0300

    Add share button

--- DallETutorial/Views/ImageDetails.swift
+++ DallETutorial/Views/ImageDetails.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ImageDetails: View {
+    private let url = URL(string: "https://www.appcoda.com")!
     @State private var quantity: String = "1"
     @State private var style: String = ""
     @State private var size: String = ""
@@ -20,15 +21,17 @@ struct ImageDetails: View {
         ZStack(alignment: .bottomTrailing) {
             Color(uiColor: .clear)//colorScheme == .light ? .white : .black)
                 .overlay(alignment: .topTrailing) {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(systemName: "x.circle.fill")
-                            .imageScale(.large)
-                            .padding(.top, 35)
-                            .padding(.trailing, 20)
-                    }
-                    .buttonStyle(.plain)
+
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image(systemName: "x.circle.fill")
+                                .padding(.trailing, 20)
+                        }
+                        .buttonStyle(.plain)
+
+                    .imageScale(.large)
+                    .padding(.top, 35)
                 }
                 .edgesIgnoringSafeArea(.all)
 
@@ -36,10 +39,15 @@ struct ImageDetails: View {
                 Spacer()
 
                 if let image = image {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 256, height: 256)
+                    HStack {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 256, height: 256)
+
+                        let shareImage = Image(uiImage: image)
+                        ShareLink(item: shareImage, preview: SharePreview("", image: shareImage))
+                    }
 
                     Button("Save Image") {
                         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
